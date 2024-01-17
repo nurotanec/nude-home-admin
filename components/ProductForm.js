369f708ff3b8807654413afde2616e935b315be3
Aidar Suleimen <36263839+nurotanec@users.noreply.github.com>
2024-01-17 17:58:37 +0600

    2:47:44

---
+++ components/ProductForm.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function ProductForm({
+  _id,
+  title: existingTitle,
+  description: existingDescription,
+  price: existingPrice,
+  images: existingImages,
+}) {
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+
+  async function saveProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+
+    if (_id) {
+      await axios.put(`/api/products`, { ...data, _id });
+    } else {
+      await axios.post("/api/products", data);
+    }
+    setGoToProducts(true);
+  }
+
+  if (goToProducts) {
+    router.push("/products");
+  }
+
+  async function uploadImages(ev) {
+    ev.preventDefault();
+
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await axios.post("/api/upload", data);
+      console.log(res);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+    }
+  }
+
+  return (
+    <form onSubmit={saveProduct}>
+      <label>Название</label>
+      <input
+        type="text"
+        name="name"
+        placeholder="название"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <label>Фото </label>
+      <div className="mb-2 flex flex-wrap gap-2">
+        {!!images?.length &&
+          images.map((link) => (
+            <div key={link} className="h-24">
+              <img src={link} alt="" className="rounded-lg" />
+            </div>
+          ))}
+        <label className="w-24 h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && <div>Нет фото</div>}
+      </div>
+      <label>Описание</label>
+      <textarea
+        name="description"
+        placeholder="описание"
+        value={description}
+        onChange={(ev) => setDescription(ev.target.value)}
+      />
+      <label>Цена (в KZT)</label>
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(ev) => setPrice(ev.target.value)}
+      />
+      <button type="submit" className="btn-primary">
+        Сохранить
+      </button>
+    </form>
+  );
+}
---
+++ pages/products/new.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
+
+export default function NewProduct() {
+  return (
+    <Layout>
+      <h1>Новый товар</h1>
+      <ProductForm />
+    </Layout>
+  );
+}
